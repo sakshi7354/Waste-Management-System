876c3c553a11485724bb08cb286c3de3a9e5a9e3
sakshi7354 <137209085+sakshi7354@users.noreply.github.com>
2025-02-04 23:33:38 +0530

    Create service.php

---
+++ service.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="with=device-width, initial-scale=1.0">
+      <title>Services</title>
+      <link rel="stylesheet" href="../css/header1.css" type="text/css"/>
+      <!--Social media icons-->
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=PT+Sans+Caption&display=swap');
+        </style>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link rel="stylesheet" href="../css/table.css">
+<link rel="stylesheet" href="../css/form.css">
+<link rel="stylesheet" href="../css/header1.css">
+<link rel="stylesheet" href="../css/footer.css">
+<link rel="stylesheet" type="text/css">
+</head>
+<body>
+     <div class="header">
+          <h1><b>Waste Management System</b></h1>
+<br>
+          <p style="color:white">Avoid pollution, be the solution! Recycle</p>
+        </div>
+        
+        <div class="navbar">
+          <a href="../index.php" class="active">Home</a>
+          <a href="../html/service.php">Services</a>
+          <a href="what_we_do.php">What we do</a>
+          <a href="../html/recycle.php">Recyle Right</a>
+          <a href="../html/About_us-edited.php">About Us</a>
+          <a href="#" >Contact Us</a>
+          <a href="https://www.google.com/maps/d/edit?mid=1Djo0nMLow_FtRCe1w7XrCEsqr4B3g9DI&usp=drive_link">find nearest center</a>
+      
+        <a href="../../../Responsive-Contact-us-form-master\Responsive-Contact-us-form-master\Responsive-Contact-us-form-master\index.php">Complain</a>
+      
+        <a href="selling\selling.html"style="color:yellow">Sell your product</a>
+        <a href="buy product\index.html" style="color:yellow">Buy product</a>
+    
+        </div>
+
+
+<br><br>
+<h1 style="color:black;" align="center"><b>OPENING SCHEDULE</b></h1>
+<br>
+<br>
+<table id="shedule">
+<tr id="firstrow">`
+<th bgcolor="green">LOCATION</th>
+<th bgcolor="green">DAY</th>
+<th bgcolor="green">TIME</th>
+<th bgcolor="green">GARBAGE TYPE</th>
+</tr>
+<tr>
+<td bgcolor="black" style="color:white">satna</td>
+<td bgcolor="black" style="color:white">monday</td>
+<td bgcolor="black" style="color:white">9 am</td>
+<td bgcolor="black" style="color:white">perishable garbage</td>
+</tr>
+<tr>
+<td bgcolor="black" style="color:white">satna</td>
+<td bgcolor="black" style="color:white">tuesday</td>
+<td bgcolor="black" style="color:white">9 am</td>
+<td bgcolor="black" style="color:white">non perishable garbage</td>
+</tr>
+<tr>
+<td bgcolor="black" style="color:white">rewa</td>
+<td bgcolor="black" style="color:white">wednesday</td>
+<td bgcolor="black" style="color:white">3 pm</td>
+<td bgcolor="black" style="color:white">perishable garbage</td>
+</tr>
+<tr>
+<td bgcolor="black" style="color:white">panna</td>
+<td bgcolor="black" style="color:white">thursday</td>
+<td bgcolor="black" style="color:white">3 pm</td>
+<td bgcolor="black" style="color:white">non perishable garbage</td>
+</tr>
+<tr>
+<td bgcolor="black" style="color:white">rewa</td>
+<td bgcolor="black" style="color:white">friday</td>
+<td bgcolor="black" style="color:white">9 am</td>
+<td bgcolor="black" style="color:white">perishable garbage</td>
+</tr>
+<tr>
+<td bgcolor="black" style="color:white">rewa</td>
+<td bgcolor="black" style="color:white">saturday</td>
+<td bgcolor="black" style="color:white">9 am</td>
+<td bgcolor="black" style="color:white">non perishable garbage</td>
+</tr>
+
+</table>
+<br><br><br><br>
+
+<!-- <div class="testbox">
+<form action="insert.php" method="post">
+<h1 align="center" style="color:black;"><b>CUSTOMER FEEDBACK FORM</b></h1>
+<br><br>
+<p style="color:blue;">Please take a few minutes to give us feedback about our service by filling in this short Customer Feedback Form. We are conducting this research in order to measure your level of satisfaction with the quality of our service. We thank you for your participation.</p>
+
+<hr />
+
+Name<input class="email" type="text name="uname"><br>
+
+Contact Number<input class="email" type="text" name="cnum">
+<h3 style="color:black;">Overall experience with our service</h3>
+
+<table>
+<tr>
+<th class="first-col"></th>
+<th bgcolor="green">Very Good</th>
+<th bgcolor="green">Good</th>
+<th bgcolor="green">Fair</th>
+<th bgcolor="green">Poor</th>
+<th bgcolor="green">Very Poor</th>
+</tr>
+<tr>
+<td class="first-col" bgcolor="green">How do you rate our service?</td>
+<td bgcolor="black"><input type="radio" value="none" name="rate" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="rate" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="rate" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="rate" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="rate" /></td>
+</tr>
+<tr>
+<td class="first-col" bgcolor="green">How is your satisfaction regarded to our service?</td>
+<td bgcolor="black"><input type="radio" value="none" name="satisfied" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="satisfied" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="satisfied" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="satisfied" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="satisfied" /></td>
+</tr>
+<tr>
+<td class="first-col" bgcolor="green">How about our customer support?</td>
+<td bgcolor="black"><input type="radio" value="none" name="name" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="name" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="name" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="name" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="name" /></td>
+</tr>
+<tr>
+<td class="first-col" bgcolor="green">How our service affect to the environment?</td>
+<td bgcolor="black"><input type="radio" value="none" name="recommend" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="recommend" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="recommend" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="recommend" /></td>
+<td bgcolor="black"><input type="radio" value="none" name="recommend" /></td>
+</tr>
+</table>
+
+<h4 style="color:black;">What should we change?(comments)</h4>
+<input class="testbox" type="text" name="comments"><br>
+
+<h4 style="color:black;">Email</h4>
+<small style="color:black;">Only if you want to hear more from us.</small>
+<input class="email" type="text" name="mail">
+
+<div class="btn-block">
+<input type="submit" name="submit" position="static">
+</div>
+</form>
+</div> -->
+
+<footer class="footer" position="static">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>Services</h4>
+          <ul>
+            <li><a href="../html/service.html">Schedule</a></li>
+            <li><a href="#">Feedback</a></li>
+          </ul>
+        </div>
+        
+        <div class="footer-col">
+          <h4>What we do</h4>
+          <ul>
+            <li><a href="../html/what_we_do.html">What we do</a></li>
+            <li><a href="#effects">Climate change</a></li>
+            <li><a href="#Life on land">Life on land</a></li>
+            <li><a href="#Life below water">Life below water</a></li>
+            <li><a href="#ocean">Our Impact</a></li>
+          </ul>
+      </div>
+      
+      <div class="footer-col">
+          <h4>Recycle Right</h4>
+          <ul>
+            <li><a href="../html/recycle.html">Waste</a></li>
+            <li><a href="../html/recycle.html">Sorting guide</a></li>
+            <li><a href="../html/recycle.html">E waste</a></li>
+          </ul>
+      </div>
+      
+      <div class="footer-col">
+           <h4>Follow Us</h4>
+           <div class="social-links">
+             <ul>
+            <li><a href="https://twitter.com"><i class="fab fa-twitter" ></i></a></i></li>
+            <li><a href="https://facebook.com"><i class="fab fa-facebook"></i></a></li>
+            <li><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+          </div>
+            
+      </div>
+    </div>
+    </div>
+  </footer>
+
+
+
+</body>
+</html>
