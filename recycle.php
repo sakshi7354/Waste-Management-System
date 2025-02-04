ee9bb9713d205280714b84f890b1c4099d77e2fd
sakshi7354 <137209085+sakshi7354@users.noreply.github.com>
2025-02-04 23:33:09 +0530

    Create recycle.php

---
+++ recycle.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recycle Right</title>
+    <link rel="stylesheet" href="../css/header.css" type="text/css">
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=PT+Sans+Caption&display=swap');
+    </style>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!--Social media icons-->
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+   
+</head>
+<body>
+    <div class="header">
+        <h1><b>Waste Management System</b></h1>
+        <p>Avoid pollution, be the solution! Recycle</p>
+    </div>
+      
+      <div class="navbar">
+      <a href="../index.php" class="active">Home</a>
+          <a href="../html/service.php">Services</a>
+          <a href="what_we_do.php">What we do</a>
+          <a href="../html/recycle.php">Recyle Right</a>
+          <a href="../html/About_us-edited.php">About Us</a>
+          <a href="#" >Contact Us</a>
+          <a href="https://www.google.com/maps/d/edit?mid=1Djo0nMLow_FtRCe1w7XrCEsqr4B3g9DI&usp=drive_link">find nearest center</a>
+      
+        <a href="../../../Responsive-Contact-us-form-master\Responsive-Contact-us-form-master\Responsive-Contact-us-form-master\index.php">Complain</a>
+      
+        <a href="selling\selling.html"style="color:yellow">Sell your product</a>
+        <a href="buy product\index.html" style="color:yellow">Buy product</a>
+    
+      </div>  
+    </div>
+
+    <section>
+        <div class="transition">
+
+            
+            <s id="s1"></s> 
+            <s id="s2"></s>
+            <s id="s3"></s>
+            <s id="s4"></s>
+          
+            <div class="slider">
+             <div class="a" style="background-image:url('../media/covimg.jpeg')">
+                
+                <h2>Everything you need to know about recycling!</h2>
+                <p>We’re here to help you recycle right, answer common recycling questions and provide the information, tools and resources you need to make recycling both simple and convenient.
+                Every time you choose to recycle, you are giving that item a second life to serve a new purpose and save natural resources. Now more than ever it’s important that only the right items, free from contamination, make their way into your recycling bin to help keep recycling, and our shared planet, sustainable.<br><br><br><br><br><br><br><br></p>
+             </div>
+                
+              <div class="b" style="background-image:url('../media/img1.jpg')">
+                      <h2>Sustainable waste management strategies</h2>
+                      <p>Waste management refers to the activities and actions that handle waste materials. It includes collection, transportation, processing, and disposal of waste. Waste prevention, recycling, reuse, and recovery are important waste management strategies that eases the burden on landfills, conserves natural resources, and saves energy.Although the world is currently experiencing increased environmental contamination with solid waste, proper management and profitable recycling can make it beneficial to the environment. This helps utilize resources more effectively and sustainably.<br><br><br><br><br><br><br><br></p>
+                  </div>
+              <div class="c" style="background-image:url('../media/ewaste.jpg')">
+                      <h2>Dispose E-waste the proper way</h2>
+                      <p>E-waste is any electrical or electronic equipment that’s been discarded. E-waste is particularly dangerous due to toxic chemicals that naturally leach from the metals inside when buried. Due to the increasing consumption of electronic goods, it’s important to keep reinforcing that message that we need to recycle the older models, not throw them out. Improper disposal of electronic waste leads to serious environmental issues. In contrast, recycling provides considerable benefits to our environment.<br><br><br><br><br><br><br><br></p>
+                  </div>
+              <div class="d" style="background-image:url('../media/sort.jpg')">
+                      <h2>Improve waste sorting and recycling</h2>
+                      <p>Waste which has not been sorted at the source is difficult or even impossible to recycle. Indeed, recycling is made easy only if material flows are of the same kind. Sorting out waste that has been mixed is not only technically very difficult, but more expensive as well, and leads to less successful recycling. Sorting out waste at the source thus allows to improve the quality of the material that is recycled and that is why we encourage and welcome you to take part in Sorting waste! <br><br><br><br><br><br><br><br></p>
+                  </div>
+            </div>
+            
+            <div class="prevNext">
+              <div><a href="#s4"></a><a href="#s2"></a></div>
+              <div><a href="#s1"></a><a href="#s3"></a></div>
+              <div><a href="#s2"></a><a href="#s4"></a></div>
+              <div><a href="#s3"></a><a href="#s1"></a></div>
+            </div>
+          
+            <div class="bullets">
+              <a href="#s1"></a>
+              <a href="#s2"></a>
+              <a href="#s3"></a>
+              <a href="#s4"></a>
+            </div>
+          
+          </div>
+    </section>
+    
+    <section>
+
+      <div class="skew-c"></div>
+      <div class="colour-block">
+        <h1> What is recycling and why recycle?</h1>
+        <p>Recycling is the method used to convert the waste materials into products which can be reused. The common waste materials which have the potential to be reused can be recycled as raw materials, can reduce energy consumption, reduce pollution, reduce further pollution of water; reducing the need for waste disposal.Recycling is the third and key component of the 3 Rs, Reduce, Reuse and Recycle. It achieves the objective of keeping materials out of the landfill by turning them back into raw materials that will be used again to manufacture new products or items.
+        </p>
+
+      </div>
+      <div class="skew-cc"></div>
+      <div class="white-block">
+        <h1>Help facilitate recycling.</h1>
+        <p>Waste which has not been sorted at the source is difficult or even impossible to recycle. Indeed, recycling is made easy only if material flows are of the same kind. Sorting out waste that has been mixed is not only technically very difficult, but more expensive as well, and leads to less successful recycling. Sorting out waste at the source thus allows to improve the quality of the material that is recycled and that is why we encourage and welcome you to take part in Sorting waste.</p>
+      </div>
+   </section>
+
+   <div class="ves" style="margin-top:30px">
+    <div class="roww">
+      <div class="colmm-12">
+        <div class="title-heading">
+          How to recycle right!
+        </div>
+        <div class="intro-para2">
+          <p>Do you often find yourself wondering how you could contribute to make the world a better and a cleaner place? 
+        <br>
+           Our objective is to make recycling an easy and convenient experince for everyone. So here are 3 fundemental tips to make recycling easier for you! </p>
+          </div>
+      </div>
+    </div>
+    <div class="roww">
+      <div class = "colmm-lg-4 colmm-sm-4 colmm-md-4 colmm-xl-4 colmm-xs-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">What you can recycle</h4>
+            <div class="card-image">
+              <img alt="Card image cap" class="card-img-top img-fluid" src="../media/3bins.png" style="width: 25%; height:auto;" />
+              <p class="card-text ">
+                Any clean bottles, paper, cardboard and cans are suitable to be recycled.
+              </p>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div class = "colmm-lg-4 colmm-sm-4 colmm-md-4 colmm-xl-4 colmm-xs-12">
+         <div class="card">
+           <div class="card-body">
+             <h4 class="card-title">What you can`t recycle</h4>
+             <div class="card-image">
+               <img alt="Card image cap" class="card-img-top img-fluid" src="../media/yellowbin.png" style="width: 17%; height:auto;" />
+             <p class="card-text">
+               Food waste and liquids cannot be recycled and should not be added together with recyclable materials.
+             </p>
+           </div>
+         </div>
+       </div>
+       <div class = "colmm-lg-4 colmm-sm-4 colmm-md-4 colmm-xl-4 colmm-xs-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">What you shouldn`t add to be recycled</h4>
+            <div class="card-image">
+              <img alt="Card image cap" class="card-img-top img-fluid" src="../media/plastic.png" style="width: 18%; height:auto;" />
+            <p class="card-text">
+              Plastic bags should not be added together with other recyclable materials as they are not considered recylable by many recycling programs due to the high number of issues surrounding them.
+            </p>
+          </div>
+        </div>
+      </div>
+  
+  
+    </div>
+  </div>
+
+</section>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="footer-col">
+            <h4>Services</h4>
+            <ul>
+              <li><a href="../html/service.html">Schedule</a></li>
+              <li><a href="#">Feedback</a></li>
+            </ul>
+          </div>
+          
+          <div class="footer-col">
+            <h4>What we do</h4>
+            <ul>
+              <li><a href="../html/what_we_do.html">What we do</a></li>
+              <li><a href="#">Climate change</a></li>
+              <li><a href="#">Life on land</a></li>
+              <li><a href="#">Life below water</a></li>
+              <li><a href="#">Our Impact</a></li>
+            </ul>
+        </div>
+        
+        <div class="footer-col">
+            <h4>Recycle Right</h4>
+            <ul>
+              <li><a href="../html/recycle.html">Waste</a></li>
+              <li><a href="#">Sorting guide</a></li>
+              <li><a href="#">E waste</a></li>
+            </ul>
+        </div>
+        
+        <div class="footer-col">
+             <h4>Follow Us</h4>
+             <div class="social-links">
+               <ul>
+              <li><a href="https://twitter.com"><i class="fab fa-twitter" ></i></a></i></li>
+              <li><a href="https://facebook.com"><i class="fab fa-facebook"></i></a></li>
+              <li><a href="https://instagram.com"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+              
+        </div>
+      </div>
+    </footer>
+    
+</body>
+</html> 
